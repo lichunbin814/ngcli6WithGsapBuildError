dbd2e01bd7989612fb3095d3eadf1c4a0c2cef98
jimmy.lee <jimmy.lee@careline.com.tw>
2018-08-04 19:53:19 +0800

    add gsap sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { TweenLite } from 'gsap/TweenMax';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+
+  @ViewChild('list')
+  listElmRef : ElementRef;
   title = 'app';
+
+
+  start(): void {
+    const element = this.listElmRef.nativeElement;
+    TweenLite.set(element, { height: 'auto', opacity: 1 });
+    TweenLite.from(element, 2000, { height: 0, opacity: 0 });
+  }
 }
